01cb130ec78ab5877e72ae1a5cd4592cac3f6fed
Aleazar yilma <yilma4060@gmail.com>
2022-07-20 00:37:28 +0300

    Chnage project card and modal to dynamic data

--- script.js
+++ script.js
@@ -1,51 +1,154 @@
-const modalhtml =
-  '<div class="modal">\
-<div class="modal-content">\
-    <header class="modal-header">\
-        <span class="modal-title"></span>\
-        <button data-close-button class="modal-close">&times;</button>\
-    </header>\
-    <div class="modal-body">\
-        <div class="modal-body-img">\
-        <img src="Images/snap.png" alt="">\
-        </div>\
-        <p class="modal-body-detail"></p>\
-        <ul class="modal-body-technologies">\
-        </ul>\
-        <div class="modal-body-actions">\
-            <button>\
-                <span>See live</span>\
-                <img src="Images/icons/seelive.png" alt="see project live">\
-            </button>\
-            <button>\
-                <span>See Source</span>\
-                <img src="Images/icons/seesource.png" alt="see project source code">\
-            </button>\
-            </div></div></div>\
-</div>';
+const projects = [
+  {
+    projectId: 1,
+    projectTitle: 'Multi-Post one',
+    description: "A daily selection of privately personalized reads; no accounts or\
+    sign-ups required. has been the industry's standard dummy text ever\
+    since the 1500s, when an unknown printer took a standard dummy text.",
+    image: 'Images/projectOne.png',
+    technologies: ['html', 'css', 'ruby'],
+    livelink: 'www.google.com',
+    soruceLink: 'wwww.google.com',
+  },
+  {
+    projectId: 2,
+    projectTitle: 'Multi-Post two',
+    description: "A daily selection of privately personalized reads; no accounts or\
+    sign-ups required. has been the industry's standard dummy text ever\
+    since the 1500s, when an unknown printer took a standard dummy text.",
+    image: 'Images/projectOne.png',
+    technologies: ['html', 'css', 'ruby'],
+    livelink: 'www.google.com',
+    soruceLink: 'wwww.google.com',
+  },
+  {
+    projectId: 3,
+    projectTitle: 'Multi-Post three',
+    description: "A daily selection of privately personalized reads; no accounts or\
+    sign-ups required. has been the industry's standard dummy text ever\
+    since the 1500s, when an unknown printer took a standard dummy text.",
+    image: 'Images/projectOne.png',
+    technologies: ['html', 'css', 'ruby'],
+    livelink: 'www.google.com',
+    soruceLink: 'wwww.google.com',
+  },
+  {
+    projectId: 4,
+    projectTitle: 'Multi-Post Four',
+    description: "A daily selection of privately personalized reads; no accounts or\
+    sign-ups required. has been the industry's standard dummy text ever\
+    since the 1500s, when an unknown printer took a standard dummy text.",
+    image: 'Images/projectOne.png',
+    technologies: ['html', 'css', 'ruby'],
+    livelink: 'www.google.com',
+    soruceLink: 'wwww.google.com',
+  },
+];
+function GetTechnologyListHtmlContent(project) {
+  let technology = '';
+  project.technologies.forEach((item) => {
+    technology += `<li class="listseparator">${item}</li>`;
+  });
+  return technology;
+}
+function GenerateCardContent(project) {
+  const technology = GetTechnologyListHtmlContent(project);
+  let result = `<li class="workCard" id="first">\
+ <div class="image_container"><img src="${project.image}" alt="projectone"></div>\
+ <div class="projectdetail">\
+     <h3>${project.projectTitle}</h3>\
+     <p> ${project.description}</p>\
+     <ul class="workTechs"> ${technology}</ul>\
+     <button data-id ="${project.projectId}"class="seebutton transition_button seedetail" type="submit">See project</button>\
+ </div>\
+</li>`;
+  if (project.projectId % 2 == 0) {
+    result = result.replace('class="workCard"', 'class="workCard odd" id="secondcard"');
+  }
+  return result;
+}
+function GenerateModalContent(project) {
+  const technology = GetTechnologyListHtmlContent(project);
+  const modalContent = `\
+  <div class="modal-content">\
+  <header class="modal-header">\
+      <span class="modal-title">${project.projectTitle}</span>\
+      <button data-close-button class="modal-close">&times;</button>\
+  </header>\
+  <div class="modal-body">\
+      <div class="modal-body-img">\
+      <img src="Images/snap.png" alt="">\
+      </div>\
+      <p class="modal-body-detail"> ${project.description} </p>\
+      <ul class="modal-body-technologies">${technology}</ul>\
+      <div class="modal-body-actions">\
+          <button>\
+              <span>See live</span>\
+              <img src="Images/icons/seelive.png" alt="see project live">\
+          </button>\
+          <button>\
+              <span>See Source</span>\
+              <img src="Images/icons/seesource.png" alt="see project source code">\
+          </button></div></div></div> \
+  `;
+  return modalContent;
+}
+function AddProjects() {
+  let htmlContent = '';
+  const projectContainer = document.querySelector('.works');
+  projects.forEach((project) => {
+    htmlContent += GenerateCardContent(project);
+  });
+  projectContainer.innerHTML = htmlContent;
+}
 
+function CloseModal() {
+  alert('hi');
+  const modal = document.querySelector('.modal');
+  modal.innerHTML = '';
+  modal.classList.add('hidden');
+}
 function OpenMobileMenu() {
-  const mobileMenu = document.querySelector("#modile_nav");
-  const main = document.querySelector("main");
-  mobileMenu.classList.remove("show_hide");
-  main.classList.add("hidden");
+  const mobileMenu = document.querySelector('#modile_nav');
+  const main = document.querySelector('main');
+  mobileMenu.classList.remove('show_hide');
+  main.classList.add('hidden');
 }
 function CloseMobileMenu() {
-  const mobileMenu = document.querySelector("#modile_nav");
-  mobileMenu.classList.add("show_hide");
-  const main = document.querySelector("main");
-  main.classList.remove("hidden");
+  const mobileMenu = document.querySelector('#modile_nav');
+  mobileMenu.classList.add('show_hide');
+  const main = document.querySelector('main');
+  main.classList.remove('hidden');
 }
-document.addEventListener("DOMContentLoaded", () => {
-  const openMenuElement = document.querySelector("#open_mobile_humberger");
-  const closeMenuElement = document.querySelector("#close_mobile_humberger");
-  const menuItems = document.querySelectorAll(".mobile_nav_items");
-  const others = document.querySelector(".mobile_nav");
+function GetProjectDetail() {
+  const projectId = this.getAttribute('data-id');
+  debugger;
+  const project = projects.find((e) => e.projectId == projectId);
+  const htmlContent = GenerateModalContent(project);
+  const modal = document.querySelector('.modal');
+  modal.innerHTML = htmlContent;
+  modal.classList.remove('hidden');
+}
 
-  others.addEventListener("click", CloseMobileMenu);
-  openMenuElement.addEventListener("click", OpenMobileMenu);
-  closeMenuElement.addEventListener("click", CloseMobileMenu);
+document.addEventListener('DOMContentLoaded', () => {
+  AddProjects();
+  const openMenuElement = document.querySelector('#open_mobile_humberger');
+  const closeMenuElement = document.querySelector('#close_mobile_humberger');
+  const menuItems = document.querySelectorAll('.mobile_nav_items');
+  const others = document.querySelector('.mobile_nav');
+  const detailProjectbuttons = document.querySelectorAll('.seedetail');
+  const closeModalButtons = document.querySelectorAll('.modal-close');
+
+  others.addEventListener('click', CloseMobileMenu);
+  openMenuElement.addEventListener('click', OpenMobileMenu);
+  closeMenuElement.addEventListener('click', CloseMobileMenu);
   menuItems.forEach((menuItem) => {
-    menuItem.addEventListener("click", CloseMobileMenu);
+    menuItem.addEventListener('click', CloseMobileMenu);
+  });
+  detailProjectbuttons.forEach((getdetail) => {
+    getdetail.addEventListener('click', GetProjectDetail);
+  });
+  closeModalButtons.forEach((close) => {
+    close.addEventListener('click', CloseModal);
   });
 });
